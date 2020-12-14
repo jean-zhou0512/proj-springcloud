1b936e9c54b7810706c8135f767cba076b07d98d
jean-zhou0512 <2434153436@qq.com>
2020-12-14 22:27:54 +0800

    add

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8803/src/main/java/com/cn/springcloud/controller/StreamListenerController.java
@@ -0,0 +1,21 @@
+package com.cn.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableBinding(Sink.class)
+public class StreamListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("消费者2号,接收: "+message.getPayload()+" \tport: "+serverPort);
+    }
+}
