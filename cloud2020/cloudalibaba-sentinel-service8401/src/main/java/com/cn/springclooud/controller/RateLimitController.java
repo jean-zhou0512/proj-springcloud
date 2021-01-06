7dc0888b4bd784dcf2b8200f826e6a6f9abd524e
zjm <2434153436@qq.com>
2021-01-06 17:09:44 +0800

    add

---
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/cn/springclooud/controller/RateLimitController.java
@@ -0,0 +1,35 @@
+package com.cn.springclooud.controller;
+
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.cn.springcloud.entities.Payment;
+import com.cn.springcloud.utils.result.CommonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RateLimitController {
+
+    @RequestMapping(value="/service/rate/byResource")
+    @SentinelResource(value = "byResource" , blockHandler = "handleException")
+    public CommonResult byResource(){
+        Payment payment = new Payment();
+        payment.setId(2021L);
+        payment.setSerial("serial001");
+        return new CommonResult(payment,200,"按资源名称限流测试成功");
+    }
+
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+
+    @RequestMapping(value="/service/rate/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl(){
+        Payment payment = new Payment();
+        payment.setId(2021L);
+        payment.setSerial("sreial002");
+        return new CommonResult(payment,200,"按url限流测试成功");
+    }
+}
--- cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/cn/springclooud/controller/TestController.java
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/cn/springclooud/controller/TestController.java
@@ -1,19 +1,65 @@
 package com.cn.springclooud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class TestController {
 
     @RequestMapping("/service/test/testA")
     public String getTestA(){
+        //暂停毫秒
+        try{
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return ".........test a";
     }
 
     @RequestMapping("/service/test/testB")
     public String getTestB(){
+        System.out.println(new Date());
         return "..........test b";
     }
+
+    @RequestMapping("/service/test/testD")
+    public String getTestD(){
+//        try{
+//            TimeUnit.SECONDS.sleep(1);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("TestD 测试RT");
+
+        System.out.println("TestD 测试异常比例");
+        int age= 10/0;
+        return "......testD";
+    }
+
+    @RequestMapping("/service/test/testE")
+    public String getTestE(){
+        System.out.println("TestE 测试异常数");
+        int age=10/0;
+        return "......testE";
+    }
+
+    @RequestMapping("/service/test/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(
+          @RequestParam(value = "p1",required = false) String p1,
+          @RequestParam(value = "p2",required = false)String p2
+    ){
+        return  "......testF";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "deal_testHotKey";
+    }
+
 
 }
