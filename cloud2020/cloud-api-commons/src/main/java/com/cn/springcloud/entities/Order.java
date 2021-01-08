8b26d6eaf4d9a991706ab21186e2e04e57c25310
zjm <2434153436@qq.com>
2021-01-08 16:01:55 +0800

    add

---
+++ cloud2020/cloud-api-commons/src/main/java/com/cn/springcloud/entities/Order.java
@@ -0,0 +1,58 @@
+package com.cn.springcloud.entities;
+
+public class Order {
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private int count;
+    private double money;
+    private int status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
